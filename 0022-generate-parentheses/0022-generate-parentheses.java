faa9cbd6d1d44d52eb80b008da60670c7547123f
YASHVARDHAN YADAV <yadavyashvardhan11@gmail.com>
2026-09-19 10:20:47 +0530

    Time: 2 ms (69.41%), Space: 44.6 MB (64.19%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list=new ArrayList<>();
+        solve(n,0,0, "", list);
+        return list;
+    }
+    public void solve(int n , int open,int close, String ans, List<String> list){
+        if(open==n && close==n){
+            list.add(ans);
+            return ;
+        }
+        if(open<n){
+            solve(n,open+1,close, ans+"(" ,list);
+        }
+        if(close<open){    
+            solve(n,open,close+1,ans+")",list);
+        }    
+    }
+}
