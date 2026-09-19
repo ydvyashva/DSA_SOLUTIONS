01341cfa6cd03225c9e24ccd80e644005d3824e2
YASHVARDHAN YADAV <yadavyashvardhan11@gmail.com>
2026-09-19 10:24:37 +0530

    Time: 2 ms (69.41%), Space: 44.5 MB (64.19%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -5,9 +5,12 @@ class Solution {
         return list;
     }
     public void solve(int n , int open,int close, String ans, List<String> list){
-        if(open==n && close==n){
+        //if(open==n && close==n){
+          //  list.add(ans);
+            //return ;
+        if(ans.length()==2*n){
             list.add(ans);
-            return ;
+            return;  
         }
         if(open<n){
             solve(n,open+1,close, ans+"(" ,list);
